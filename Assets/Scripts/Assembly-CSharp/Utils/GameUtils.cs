4ae58271ced3a0df8dd9309b815fe646a60a2089
WALLE <trungdq.social@gmail.com>
2024-04-04 00:34:37 +0700

    Still not able to see him

--- Assets/Scripts/Assembly-CSharp/Utils/GameUtils.cs
+++ Assets/Scripts/Assembly-CSharp/Utils/GameUtils.cs
@@ -8,6 +8,7 @@ namespace Assets.Scripts.Assembly_CSharp.Utils
 {
     public static class GameUtils
     {
+        [PunRPC]
         public static T InstantiateAsGameObject<T>(UnityEngine.Object obj) where T : class
         {
             if (IsMultiplayer())
@@ -19,10 +20,11 @@ namespace Assets.Scripts.Assembly_CSharp.Utils
             return UnityEngine.Object.Instantiate(obj) as T;
         }
 
+        [PunRPC]
         public static T InstantiateAsGameObject<T>(UnityEngine.Object obj, Vector3 vector3, Quaternion quaternion) where T : class
         {
             if (IsMultiplayer())
-                return PhotonView.Instantiate(obj) as T;
+                return PhotonView.Instantiate(obj, vector3, quaternion) as T;
 
             return UnityEngine.Object.Instantiate(obj, vector3, quaternion) as T;
         }
